26a26ee30a86f6b99f951f65adbd38829c312149
Ashfaq <mybusinessisduty482@gmail.com>
2025-03-15 15:06:07 +0500

    cors

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
