003b17f0bfe0f58c34f1bb02a90ebe6eb5f70689
Ashfaq <mybusinessisduty482@gmail.com>
2025-03-15 17:55:37 +0500

    change in jwt

--- server/middlewares/verify-jwt.js
+++ server/middlewares/verify-jwt.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRET_KEY } from '../config/index.js';
 
 const verifyJwt = async (req, res, next) => {
-    const token = req.cookies?.token;
+    const token = await req.cookies?.token;
     if (!token) {
         return res.status(401).json({ message: 'Please login to continue.' });
     }
