c4bf6c1ef7c77620e4bab6d3347fcdfccfbdf145
Ashfaq <mybusinessisduty482@gmail.com>
2025-03-15 18:09:10 +0500

    jwt chjanges

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -24,7 +24,7 @@ const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
         res.json({ message: 'You logged in successfully', Token: token, user: userWithOutPassword });
