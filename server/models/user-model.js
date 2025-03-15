c1724e2d2bc11b3bc84c20826da0e2a0a72a3472
Ashfaq <mybusinessisduty482@gmail.com>
2025-03-15 14:33:32 +0500

    db connection fixing

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,9 +1,17 @@
 import mongoose from 'mongoose';
 import { MONGO_URI } from '../config/index.js';
 const { Schema, model } = mongoose;
-
-// Create database
-mongoose.connect(MONGO_URI);
+// const MONGO_URI = "mongodb+srv://AshfaqJaspal:Ashfaq%4032643264@employee-cluster.gn8ln.mongodb.net/Employee-management-app?retryWrites=true&w=majority&appName=Employee-Cluster";
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log('✅ MongoDB connected successfully');
+        process.exit(0); // Exit the process after successful connection
+    })
+    .catch((err) => {
+        console.error('❌ MongoDB connection error:', err);
+        process.exit(1); // Exit with an error
+    });
 
 // Create Schema
 const userSchema = new Schema({
