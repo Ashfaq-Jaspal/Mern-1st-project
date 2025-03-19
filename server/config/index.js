e85d0fe2998f13948ea634730464927535e7d0f5
Ashfaq <mybusinessisduty482@gmail.com>
2025-03-19 23:16:56 +0500

    setting cors options

--- server/config/index.js
+++ server/config/index.js
@@ -4,5 +4,6 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const FRONTEND_URL = process.env.FRONTEND_URL
 
-export { PORT, MONGO_URI, JWT_SECRET_KEY };
+export { PORT, MONGO_URI, JWT_SECRET_KEY, FRONTEND_URL };
--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ import cors from 'cors';
 import express from 'express';
 const app = express();
 import router from './routes/index.js';
-import { PORT } from './config/index.js';
+import { FRONTEND_URL, PORT } from './config/index.js';
 import cookieParser from 'cookie-parser';
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Allow frontend URL
+    origin: ['http://localhost:5173',FRONTEND_URL], // Allow frontend URL
     credentials: true, // Allow cookies (important for authentication)
     methods: 'GET,POST,PUT,DELETE', // Allow these HTTP methods
     allowedHeaders: 'Content-Type,Authorization', // Allow these headers
