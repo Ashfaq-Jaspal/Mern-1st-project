e5c35f2555d5031cbe68bca40340f14e35ec7cf2
Ashfaq <mybusinessisduty482@gmail.com>
2025-03-20 17:54:20 +0500

    cleaning

--- client/src/pages/adminPages/CreateProject.jsx
+++ client/src/pages/adminPages/CreateProject.jsx
@@ -74,130 +74,133 @@ const CreateProject = () => {
             });
         }
     }
-    return (
-        <div className="items-center">
-            <form
-                action="/create-project"
-                method="post"
-                onSubmit={(e) => {
-                    submitHandler(e);
-                }}
-                className="h-[80vh] text-2xl flex flex-col px-10  rounded-lg gap-2 border border-emerald-900 items-center justify-center"
-            >
-                <div className="flex gap-10">
-                    <div className="flex flex-col gap-5 pt-8">
-                        <input
-                            onChange={(e) => {
-                                setProjectName(e.target.value);
-                            }}
-                            name="projectName"
-                            value={projectName}
-                            type="text"
-                            required
-                            placeholder="Project name"
-                            className="text-gray-400 italic border border-emerald-900 outline-none bg-transparent px-3 py-2 rounded-lg w-80"
-                        />
-                        <textarea
-                            onChange={(e) => {
-                                setDescription(e.target.value);
-                            }}
-                            name="description"
-                            value={description}
-                            required
-                            placeholder="Project description"
-                            className="text-gray-400 italic border border-emerald-900 outline-none bg-transparent px-3 max-h-32 min-h-32 py-1 rounded-lg w-80"
-                        />
-                    </div>
-                    <div className="flex flex-col gap-4">
-                        <label className="text-green-500 text-lg">
-                            Start date
-                            <br />
+    
+    if (!loading) {
+        return (
+            <div className="items-center">
+                <form
+                    action="/create-project"
+                    method="post"
+                    onSubmit={(e) => {
+                        submitHandler(e);
+                    }}
+                    className="h-[80vh] text-2xl flex flex-col px-10  rounded-lg gap-2 border border-emerald-900 items-center justify-center"
+                >
+                    <div className="flex gap-10">
+                        <div className="flex flex-col gap-5 pt-8">
                             <input
                                 onChange={(e) => {
-                                    setStartDate(e.target.value);
+                                    setProjectName(e.target.value);
                                 }}
-                                name="startDate"
-                                value={startDate}
-                                type="date"
+                                name="projectName"
+                                value={projectName}
+                                type="text"
                                 required
-                                className="w-80 italic border border-emerald-900 text-gray-400 bg-transparent text-2xl p-1.5 rounded-lg focus:outline-none "
+                                placeholder="Project name"
+                                className="text-gray-400 italic border border-emerald-900 outline-none bg-transparent px-3 py-2 rounded-lg w-80"
                             />
-                        </label>
-                        <label className="text-red-600 text-lg">
-                            Due date
-                            <br />
-                            <input
+                            <textarea
                                 onChange={(e) => {
-                                    setEndDate(e.target.value);
+                                    setDescription(e.target.value);
                                 }}
-                                name="endDate"
-                                value={endDate}
-                                type="date"
+                                name="description"
+                                value={description}
                                 required
-                                className="w-80 italic border  border-emerald-900 text-gray-400 bg-transparent text-2xl p-1.5 rounded-lg focus:outline-none"
+                                placeholder="Project description"
+                                className="text-gray-400 italic border border-emerald-900 outline-none bg-transparent px-3 max-h-32 min-h-32 py-1 rounded-lg w-80"
                             />
-                        </label>
-                        {loading ? (
-                            <>
-                                <h1>Fetching...</h1>
-                            </>
-                        ) : (
-                            <>
-                                <Select
-                                    className="w-80 text-2xl"
-                                    options={formattedEmployeesForReactSelect}
-                                    value={selectedEmployees}
-                                    placeholder="Select employees..."
-                                    isMulti
-                                    onChange={(selected) => setSelectedEmployees(selected)}
-                                    styles={{
-                                        control: (base) => ({
-                                            ...base,
-                                            fontSize: '1.5rem',
-                                            background: 'transparent',
-                                            borderColor: '#064e3b',
-                                            padding: '2px',
-                                            borderRadius: '8px',
-                                            cursor: 'pointer',
-                                        }),
-                                        placeholder: (base) => ({
-                                            ...base,
-                                            color: '#9ca3af',
-                                            fontStyle: 'italic',
-                                        }),
-                                        dropdownIndicator: (base) => ({
-                                            ...base,
-                                            color: 'gray',
-                                        }),
-                                        menu: (base) => ({
-                                            ...base,
-                                            backgroundColor: 'white',
-                                            border: '1px solid gray',
-                                        }),
-                                        option: (base, { isFocused, isSelected }) => ({
-                                            ...base,
-                                            fontSize: '1.25rem',
-                                            backgroundColor: isSelected ? 'blue' : isFocused ? 'lightgray' : 'white',
-                                            cursor: 'pointer',
-                                        }),
+                        </div>
+                        <div className="flex flex-col gap-4">
+                            <label className="text-green-500 text-lg">
+                                Start date
+                                <br />
+                                <input
+                                    onChange={(e) => {
+                                        setStartDate(e.target.value);
                                     }}
-                                ></Select>
-                            </>
-                        )}
+                                    name="startDate"
+                                    value={startDate}
+                                    type="date"
+                                    required
+                                    className="w-80 italic border border-emerald-900 text-gray-400 bg-transparent text-2xl p-1.5 rounded-lg focus:outline-none "
+                                />
+                            </label>
+                            <label className="text-red-600 text-lg">
+                                Due date
+                                <br />
+                                <input
+                                    onChange={(e) => {
+                                        setEndDate(e.target.value);
+                                    }}
+                                    name="endDate"
+                                    value={endDate}
+                                    type="date"
+                                    required
+                                    className="w-80 italic border  border-emerald-900 text-gray-400 bg-transparent text-2xl p-1.5 rounded-lg focus:outline-none"
+                                />
+                            </label>
+                            {loading ? (
+                                <>
+                                    <h1>Fetching...</h1>
+                                </>
+                            ) : (
+                                <>
+                                    <Select
+                                        className="w-80 text-2xl"
+                                        options={formattedEmployeesForReactSelect}
+                                        value={selectedEmployees}
+                                        placeholder="Select employees..."
+                                        isMulti
+                                        onChange={(selected) => setSelectedEmployees(selected)}
+                                        styles={{
+                                            control: (base) => ({
+                                                ...base,
+                                                fontSize: '1.5rem',
+                                                background: 'transparent',
+                                                borderColor: '#064e3b',
+                                                padding: '2px',
+                                                borderRadius: '8px',
+                                                cursor: 'pointer',
+                                            }),
+                                            placeholder: (base) => ({
+                                                ...base,
+                                                color: '#9ca3af',
+                                                fontStyle: 'italic',
+                                            }),
+                                            dropdownIndicator: (base) => ({
+                                                ...base,
+                                                color: 'gray',
+                                            }),
+                                            menu: (base) => ({
+                                                ...base,
+                                                backgroundColor: 'white',
+                                                border: '1px solid gray',
+                                            }),
+                                            option: (base, { isFocused, isSelected }) => ({
+                                                ...base,
+                                                fontSize: '1.25rem',
+                                                backgroundColor: isSelected ? 'blue' : isFocused ? 'lightgray' : 'white',
+                                                cursor: 'pointer',
+                                            }),
+                                        }}
+                                    ></Select>
+                                </>
+                            )}
+                        </div>
                     </div>
-                </div>
-
-                <div className="flex">
-                    <button
-                        type="submit"
-                        className="px-3 text-2xl py-1.5 mt-2 w-80 text-gray-300 bg-emerald-900 border-none rounded-lg"
-                    >
-                        Create project
-                    </button>
-                </div>
-            </form>
-        </div>
-    );
+    
+                    <div className="flex">
+                        <button
+                            type="submit"
+                            className="px-3 text-2xl py-1.5 mt-2 w-80 text-gray-300 bg-emerald-900 border-none rounded-lg"
+                        >
+                            Create project
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 };
 
 export default CreateProject;
--- client/src/pages/adminPages/CreateUser.jsx
+++ client/src/pages/adminPages/CreateUser.jsx
@@ -65,90 +65,92 @@ function SignUp() {
         fetchUser();
     };
 
-    return (
-        <form
-            action="/create-user"
-            method="post"
-            onSubmit={(e) => {
-                submitHandler(e);
-            }}
-            className="flex flex-col text-2xl px-36 py-12 rounded-xl gap-4 border border-emerald-900 items-center justify-center"
-        >
-            <input
-                onChange={(e) => {
-                    setName(e.target.value);
+    if (!loading) {
+        return (
+            <form
+                action="/create-user"
+                method="post"
+                onSubmit={(e) => {
+                    submitHandler(e);
                 }}
-                name="name"
-                value={name}
-                type="text"
-                required
-                placeholder="Enter full name"
-                className="text-white border border-emerald-900 outline-none text-md bg-transparent px-3 py-2 rounded-lg w-96"
-            />
-            <input
-                onChange={(e) => {
-                    setEmail(e.target.value);
-                }}
-                name="email"
-                value={email}
-                type="email"
-                required
-                placeholder="Enter email"
-                className="text-white border border-emerald-900 outline-none text-md bg-transparent px-3 py-2 rounded-lg w-96"
-            />
-            <input
-                onChange={(e) => {
-                    setPassword(e.target.value);
-                }}
-                name="password"
-                value={password}
-                type="password"
-                required
-                placeholder="Enter password"
-                className="text-white border border-emerald-900 outline-none text-md bg-transparent px-3 py-2 rounded-lg w-96"
-            />
-
-            <Select
-                className="w-full"
-                options={statusOptions}
-                value={status}
-                placeholder="Select status..."
-                onChange={(selected) => setStatus(selected)}
-                styles={{
-                    control: (base) => ({
-                        ...base,
-                        background: 'transparent',
-                        borderColor: '#064e3b',
-                        padding: '2px',
-                        borderRadius: '8px',
-                        cursor: 'pointer',
-                    }),
-                    placeholder: (base) => ({
-                        ...base,
-                        color: '#9ca3af',
-                        fontStyle: 'italic',
-                    }),
-                    dropdownIndicator: (base) => ({
-                        ...base,
-                        color: 'gray',
-                    }),
-                    menu: (base) => ({
-                        ...base,
-                        backgroundColor: 'white',
-                        border: '1px solid gray',
-                    }),
-                    option: (base, { isFocused, isSelected }) => ({
-                        ...base,
-                        backgroundColor: isSelected ? 'blue' : isFocused ? 'lightgray' : 'white',
-                        cursor: 'pointer',
-                    }),
-                }}
-            ></Select>
-            <button type="submit" className="px-3 py-2 mt-5 w-80 text-white bg-emerald-900 border-none rounded-lg">
-                Create user
-            </button>
-        </form>
-    );
+                className="flex flex-col text-2xl px-36 py-12 rounded-xl gap-4 border border-emerald-900 items-center justify-center"
+            >
+                <input
+                    onChange={(e) => {
+                        setName(e.target.value);
+                    }}
+                    name="name"
+                    value={name}
+                    type="text"
+                    required
+                    placeholder="Enter full name"
+                    className="text-white border border-emerald-900 outline-none text-md bg-transparent px-3 py-2 rounded-lg w-96"
+                />
+                <input
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                    name="email"
+                    value={email}
+                    type="email"
+                    required
+                    placeholder="Enter email"
+                    className="text-white border border-emerald-900 outline-none text-md bg-transparent px-3 py-2 rounded-lg w-96"
+                />
+                <input
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    name="password"
+                    value={password}
+                    type="password"
+                    required
+                    placeholder="Enter password"
+                    className="text-white border border-emerald-900 outline-none text-md bg-transparent px-3 py-2 rounded-lg w-96"
+                />
+    
+                <Select
+                    className="w-full"
+                    options={statusOptions}
+                    value={status}
+                    placeholder="Select status..."
+                    onChange={(selected) => setStatus(selected)}
+                    styles={{
+                        control: (base) => ({
+                            ...base,
+                            background: 'transparent',
+                            borderColor: '#064e3b',
+                            padding: '2px',
+                            borderRadius: '8px',
+                            cursor: 'pointer',
+                        }),
+                        placeholder: (base) => ({
+                            ...base,
+                            color: '#9ca3af',
+                            fontStyle: 'italic',
+                        }),
+                        dropdownIndicator: (base) => ({
+                            ...base,
+                            color: 'gray',
+                        }),
+                        menu: (base) => ({
+                            ...base,
+                            backgroundColor: 'white',
+                            border: '1px solid gray',
+                        }),
+                        option: (base, { isFocused, isSelected }) => ({
+                            ...base,
+                            backgroundColor: isSelected ? 'blue' : isFocused ? 'lightgray' : 'white',
+                            cursor: 'pointer',
+                        }),
+                    }}
+                ></Select>
+                <button type="submit" className="px-3 py-2 mt-5 w-80 text-white bg-emerald-900 border-none rounded-lg">
+                    Create user
+                </button>
+            </form>
+        );
+    }
 }
 
 export default SignUp;
