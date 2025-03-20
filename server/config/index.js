0e775ab9e44f9d7ff932669e31e2074ce6c05cfe
Ashfaq <mybusinessisduty482@gmail.com>
2025-03-20 11:55:58 +0500

    a

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const FRONTEND_URL = process.env.FRONTEND_URL
